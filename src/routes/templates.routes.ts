26ca9de0f5ec0d74896a60a92d3f2880089c836a
Mariia Tararaeva <mariatararaeva@icloud.com>
2025-04-01 22:36:25 +0200

    seeding complete, templates can be retrived and the invites successfully created

--- src/routes/templates.routes.ts
+++ src/routes/templates.routes.ts
@@ -2,16 +2,18 @@ import { Router, Request, Response, NextFunction } from 'express'
 import prisma from '../../prisma/client' // Adjust path to your Prisma client
 
 const router = Router()
+console.log("Templates route loaded")
 
 // GET /api/templates
 router.get('/', async (req, res, next) => {
+  console.log("Fetching all templates")
   try {
-    const templates = await prisma.template.findMany({
+    const template = await prisma.template.findMany({
       include: {
         fields: true
       }
     })
-    res.json(templates)
+    res.json(template)
   } catch (err) {
     next(err)
   }
@@ -19,12 +21,17 @@ router.get('/', async (req, res, next) => {
 
 // GET /api/templates/:id
 router.get('/:id', async (req: Request, res: Response, next: NextFunction): Promise<void> => {
+  const id = Number(req.params.id)
+  if (isNaN(id)) {
+    res.status(400).json({ message: 'Invalid template ID format (must be a number)' })
+    return
+  }
   try {
-    const id = Number(req.params.id)
     const template = await prisma.template.findUnique({
       where: { id },
       include: { fields: true }
     })
+    console.log('template from DB:', template)
     if (!template) {
        res.status(404).json({ message: 'Template not found' })
        return
